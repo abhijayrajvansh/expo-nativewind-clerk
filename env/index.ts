3039891ca9ff562722a49b9fa5a34ee4f91e44ba
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2025-10-04 22:29:22 +0530

    update: added env example & manager

---
+++ env/index.ts
@@ -0,0 +1,28 @@
+// clerk auth
+const EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const env = {
+  EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+};
+
+export default env;
+
+console.log({EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY})
